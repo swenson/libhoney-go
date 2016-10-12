c256f48b0527da027256eca4d704bb4305791871
Christine Yen <cyen@christineyen.com>
2016-10-12 15:08:16 -0700

    JSON serialization of batch, so we send individual responses down the responses channel

--- libhoney_test.go
+++ libhoney_test.go
@@ -2,6 +2,7 @@ package libhoney
 
 import (
 	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io/ioutil"
@@ -599,6 +600,20 @@ func TestChannelMembers(t *testing.T) {
 	testEquals(t, ev4.data["B"], nil)
 	testEquals(t, ev4.data["C"], nil)
 	testEquals(t, ev4.data["D"], 2)
+}
+
+func TestEventMarshal(t *testing.T) {
+	e := Event{SampleRate: 1}
+	e.data = map[string]interface{}{"a": 1}
+	b, err := json.Marshal(e)
+	testOK(t, err)
+	testEquals(t, string(b), `{"data":{"a":1}}`)
+
+	e.Timestamp = time.Unix(1476309645, 0).UTC()
+	e.SampleRate = 5
+	b, err = json.Marshal(e)
+	testOK(t, err)
+	testEquals(t, string(b), `{"data":{"a":1},"samplerate":5,"time":"2016-10-12T22:00:45Z"}`)
 }
 
 //
